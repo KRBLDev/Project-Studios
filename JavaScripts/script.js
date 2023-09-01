bd9246444d1a72f13aee809023f47cacb74144bb
/JusttMeAlex <130336663+JusttMeAlex@users.noreply.github.com>
2023-09-01 18:15:20 +0800

    script.js

--- JavaScripts/script.js
+++ JavaScripts/script.js
@@ -31,3 +31,25 @@ window.addEventListener("load", () => {
 })
 
 console.log()
+
+var countDownDate = new Date("Sept 11, 2023 00:00:00").getTime();
+
+var x = setInterval(function() {
+
+  var now = new Date().getTime();
+
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("CountdownNet").innerHTML = "Happy 1 Year Anniversary Project Studios";
+  }
+}, 1000);
